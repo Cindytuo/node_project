e838ae1f260a0a8f6f5bbecf00dfa4fc8aa7dd84
cindy <themistuo@126.com>
2020-10-31 09:53:10 +0800

    feat: init node_project

---
+++ Event Loop/index.js
@@ -0,0 +1,30 @@
+console.log("start");
+setTimeout(() => {
+  console.log("timer1");
+  Promise.resolve().then(function () {
+    console.log("promise1");
+  });
+}, 0);
+setTimeout(() => {
+  console.log("timer2");
+  Promise.resolve().then(function () {
+    console.log("promise2");
+  });
+}, 0);
+Promise.resolve().then(function () {
+  console.log("promise3");
+});
+console.log("end");
+
+process.nextTick(() => {
+  console.log("nextTick");
+  process.nextTick(() => {
+    console.log("nextTick");
+    process.nextTick(() => {
+      console.log("nextTick");
+      process.nextTick(() => {
+        console.log("nextTick");
+      });
+    });
+  });
+});
